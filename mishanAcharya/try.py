04edbe8145c5b1d44644ec75a6d337a4e85990ed
mishana333 <mishanacharya@gmail.com>
2024-07-11 07:28:22 +0545

    try

---
+++ mishanAcharya/try.py
@@ -0,0 +1,13 @@
+def tryExcpt():
+    file_name=input('Please Enter the name of the file to read: ')
+
+    try:
+        with open(file_name, 'r') as file:
+               for x in file:
+                    print(f'content of {file_name}  is :', x )
+    except FileNotFoundError:
+        print(f"Error: The file '{file_name}' was not found.")
+    except IOError as e:
+        print(f"Error: An IOError occurred in '{file_name}'.")
+
+tryExcpt()
--- mishanAcharya/vote.py
+++ mishanAcharya/vote.py
@@ -3,7 +3,10 @@ while True:
         age=int(input('enter the age of he/she: '))
         if age> 18:
             print('He/She can vote.........')
+            break   
         else:
-            print('He/She cannot vote.......')  
+            print('He/She cannot vote.......')
+        break      
     except ValueError:
         print('Error: please enter valid age. Please try again')
+    
