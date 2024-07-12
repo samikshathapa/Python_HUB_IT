7dc8dbabdd1c42a65324dd4127946bbedb3c6278
Karuna-Bashyal00 <karunabashyal914@gmail.com>
2024-07-12 08:38:39 +0545

    class.py

--- karuna bashyal/class.py
+++ karuna bashyal/class.py
@@ -1,11 +1,20 @@
-class hero:
-    x=3
-    #method
-    def karuna():
-        print('function')
 
-print(hero)
 
-#function
-def karuna():
-    print('function')
+class person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def have_birthday(self):
+        self.age += 1
+
+    def display_info(self):
+        print(f'name= {self.name}, age={self.age}')
+
+person1 = person('karuna', 19)
+print('before birthday:')
+person1.display_info()
+
+person1.have_birthday()
+print('after birthday:')
+person1.display_info()
